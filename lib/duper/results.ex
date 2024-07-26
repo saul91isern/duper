d53756c412e79434fe5a49e53cbc6005fc154d8d
saul.isern <saulisern@gmail.com>
2024-07-26 18:56:59 +0900

    fix: tuple

--- lib/duper/results.ex
+++ lib/duper/results.ex
@@ -71,7 +71,7 @@ defmodule Duper.Results do
 
   defp hashes_with_more_than_one_path(results) do
     results
-    |> Enum.filter(fn _hash, paths -> length(paths) > 1 end)
+    |> Enum.filter(fn {_hash, paths} -> length(paths) > 1 end)
     |> Enum.map(&elem(&1, 1))
   end
 end
